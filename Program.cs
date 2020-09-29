9daa4103703ca7d2dfcef392f30d3f0056554f22
Brooke McBain <mcbainbrooke@gmail.com>
2020-09-29 14:37:12 -0500

    new project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Custom_List_Class_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
